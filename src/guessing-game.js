f342b532c85e5b376a2b820ffa5c768a513677b0
Ekaterina Mikhailova <ekaterinavorobyova20@gmail.com>
2023-02-20 11:21:34 +0300

    fix code

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,10 +1,8 @@
 class GuessingGame {
-    numb=this.number;
     constructor(guessing, max, min) {
         this.guessing = 0;
         this.max = 0;
-        this.min = 0;
-       
+        this.min = 0; 
     }
 
     setRange(min, max) {
@@ -13,27 +11,20 @@ class GuessingGame {
     }
 
     guess() {
-
             this.guessing = Math.ceil((this.max + this.min)/2); 
 
-            console.log(`рандом: ${this.guessing}`);
-            console.log(`numb:${this.numb}`);
-            console.log(`number: ${this.number}`);
-            if (this.number == this.guessing) {return this.guessing}
-            else if (this.guessing > this.numb) {this.lower()}
+            if (this.guessing > this.numb) {this.lower()}
             else if (this.guessing < this.numb) {this.greater()}
             else return this.guessing;
     }
 
     lower() {
-       console.log('low');
        this.max = this.guessing;
         this.setRange(this.min, this.max);
         this.guess();
     }
 
     greater() {
-        console.log('high');
         this.min = this.guessing;
         this.setRange(this.min, this.max);
         this.guess();
